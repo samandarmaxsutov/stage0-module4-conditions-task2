a7ff02b3d1e1171691baf980c9355de08971eaf3
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-10 10:08:06 +0500

    finished

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-
+        if (500>from1UpTo1000)
+            System.out.println("Eagle");
+        else System.out.println("Tail");
     }
 }
